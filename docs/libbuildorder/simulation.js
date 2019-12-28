516018f66a2c6dca0b0e31552f9c5d4abe2d7e78
Eren121 <frantze.raphael@gmail.com>
2019-12-28 18:16:11 +0100

    move public/ to docs/ for GitHub public pages

---
+++ docs/libbuildorder/simulation.js
@@ -0,0 +1,221 @@
+/**
+ * A build order is static, it's data that don' change
+ * The simulation will follow a Build Order
+ * To know if the build order is possible, trying to be as close to in-game as possible
+ */
+
+ // Represents an Event in the simulation.
+ // Each action has a begining and an end, also the Event can bo of two sort: a begin event or an end event.
+ class Event {
+    constructor(action, time) {
+        as(action, Action);
+        abstract(new.target, Event);
+        this.action = action;
+        this.time = time;
+    }
+
+    trigger(simulation) {
+        throw new TypeError("Event::trigger() not implemented: Derived Class does not implemented abstract method Event::trigger() of abstract class Event");
+    }
+ }
+
+ class StartActionEvent extends Event {
+    constructor(action) {
+        super(action, action.getTime());
+    }
+
+    trigger(simulation) {
+        this.action.started(simulation);
+    }
+ }
+
+ class CompletedActionEvent extends Event {
+    constructor(action) {
+        super(action, action.getTime() + action.getDuration());
+    }
+
+    trigger(simulation) {
+        this.action.completed(simulation);
+    }
+ }
+
+class Simulation {
+    constructor(bo) {
+        as(bo, BuildOrder);
+
+        this.bo = bo;
+        this.time = 0;                          // Current second count
+        this.mineral = 0;                      // Current minerals count
+        this.gas = 0;                           // Current gas count
+        this.supply = 0;                       // Current supply count
+        this.max_supply = 0;                   // Current maximum supply
+        this.mineral_harvesters = 0;           // Current harvesters at minerals
+        this.gas_harvesters = 0;                // Current harvesters at gas
+        this.units = [];                        // All units at this time of the build order, like: ["SCV", "Barracks", "Reaper", "Marine", ...] array of string
+    }
+
+    /** 
+     * Return true, if is it possible to build an unit at this point of time
+     * Regarding the supply count and the resources
+     * Don't regard if there is an avalaible building if it's produced from a building
+     */
+    hasEnoughResourcesForUnit(unit) {
+        let ok = false;
+        const cost = unit.getCost();
+
+        if(this.mineral >= cost.mineral && this.gas >= cost.gas) {
+            // Enough resources, check if enough supply
+
+            if(cost.supply <= 0 || this.supply + cost.supply <= this.max_supply) {
+                ok = true;
+            }
+        }
+
+        return ok;
+    }
+
+    /**
+     * Reset simulation until second is reached
+     */
+    goToSecond(s) {
+        as(s, Number);
+        let i;
+        
+        this.start();
+    
+        for(i = 0; i < s; i++) {
+            this.advanceToNextSecond();
+        }
+    }
+
+    // Update current supply from all units
+    updateSupply() {
+        let max = 0, total = 0;
+        let i, s;
+        let unit;
+
+        for(i = 0; i < this.units.length; i++) {
+            unit = Unit.fromName(this.units[i]);
+            s = unit.getCost().supply;
+
+            if(s < 0) {
+                max += -s;
+            } else {
+                total += s;
+            }
+        }
+
+        this.supply = total;
+        this.max_supply = max;
+    }
+
+    _buildEvents(priority_queue) {
+        
+        this.bo.visitActions(function(a) {
+            priority_queue.push(new StartActionEvent(a));
+
+            // Add completed action event only if the duration is not zero, that is if the action is not instant
+            // If the action is instant it doesn't need a finished event
+            if(a.getDuration() > 0) {
+                priority_queue.push(new CompletedActionEvent(a));
+            }
+        });
+    }
+
+    start() {
+        let i;
+
+        this.time = 0;
+        this.mineral = 50;
+        this.gas = 0;
+        this.mineral_harvesters = 12;
+        this.gas_harvesters = 0;
+        this.units = [];
+        
+        // Priority queue of all actions
+        // All actions have events that are prebuild into the priority_queue
+        // The events are not dynamically added: we know, when we build an unit, when it will be finished
+        // Because the production time is constant
+        // Also, we can add all events at the begining
+        // All events have not a finish event: we consider that, if the event has a duration of zero, that the finish event will not be called
+        // Because the event is instant.
+
+        this.priority_queue = new SortedArray([], function(a, b) {
+            return a.time - b.time;
+        });
+        this._buildEvents(this.priority_queue);
+
+        this.units.push("Command Center");
+
+        for(i = 0; i < 12; i++) {
+            this.units.push("SCV");
+        }
+
+        this.updateSupply();
+    }
+
+    // Poll an event if next event is now
+    // Returns null if there is no event to be done now
+    _pollEvent() {
+        const queue = this.priority_queue;
+        let e = null;
+
+        if(queue.length > 0 && queue.peek().time === this.time) {
+            e = queue.pop();
+        }
+
+        return e;
+    }
+
+    advanceToNextSecond() {
+        let e;
+
+        // Update resources
+
+        /**
+         * Gas mining cannot be optimised: 3 workers per gas
+         * Mineral can be optimised: there is near and far mineral patches.
+         * With a high number of workers, the collection resource rate can be approximed.
+         * But in very early game, when the number of workers is very low,
+         * the optmisation is possible and has a big impact of the mining resource collection rate:
+         * we usually see pro players trying to optimise the builds.
+         * We considers, in this build order, that the optmisation is perfect, even if there is approximiation:
+         * Under 12 scv, we consider that the optimisation is perfect with an approximat. count of short mineral patches.
+         * The optimisation, without taking others factors into consideration like spamming for that the workers don't move
+         * to another patch that can be further, is that we consider that all mineral patch that are clostest are mining
+         * in priority.
+         * 
+         * If the factor 40.0 / 60.0 was used, some build order were considered impossible by the simulation.
+         * A build order is considered impossible when, at a given point of time, one of the following condition is not matched:
+         *  - mineral >= 0
+         *  - gas >= 0
+         *  - supply <= supplyCap
+         */
+        let rate;
+
+        if(this.mineral_harvesters <= 16) {
+            rate = 45.0 / 60.0;
+        } else {
+            rate = 40.0 / 60.0;
+        }
+
+        this.mineral += this.mineral_harvesters * rate; // A mineral worker 40. min. per minute
+        this.gas += this.gas_harvesters * (38.0 / 60.0); // A gas worker 38. gaz per minute
+
+        
+        // Updates actions: check if another action is available
+        // And if current next action is at this point of time
+
+        while((e = this._pollEvent()) !== null) {
+            e.trigger(this);
+        }
+        
+        // Update time
+
+        this.time++;
+    }
+
+    isCompleted() {
+        return this.priority_queue.length === 0;
+    }
+}
---
+++ docs/main.js
@@ -0,0 +1,130 @@
+$(document).ready(function() {
+
+    $.ajaxSetup({ cache: false });
+
+    Unit.preload('../../json/units.json', function() {
+
+        $.getJSON('./../../examples/example-bo-1.json', function(json) {
+            let i = 0;
+
+            const bo = BuildOrder.fromJSON(json);
+            const simulation = new Simulation(bo);
+            const view = new BuildOrderView(bo);
+
+            Unit.loadInto($('.bo-unit'));
+
+            bo.subscribe('change', function() {
+                $('#timeline input[type=range]')
+                    .attr('max', bo.duration())
+                    .change(); // Force update simulation info.
+            });
+
+            bo.subscribe('change', function() {
+                view.renderInto($('table'));
+            });
+
+            // When he unit to build change, search available buildings, that is a row, where to build
+
+            $('.bo-unit').change(function(e) {
+                const unit = Unit.fromName($(this).val());
+                let i;
+                let action;
+
+                $('.bo-row').empty();
+
+                if(unit.hasParent()) {
+                    // Search a row where the building corresponds
+                    for(i = 0; i < bo.getActions().length; i++) {
+                        action = bo.getActions()[i];
+
+                        if(action instanceof Build && action.getUnit() === unit.getParent()) {
+                            $('.bo-row').append(
+                                $('<option>')
+                                    .val(i)
+                                    .text(i + 1)
+                            );
+                        }
+                    }
+                } else {
+                    // If the unit is built from scratch, the only available option is create a new row.
+                    // The collapse is not taken into consideration
+                    $('.bo-row').html($('<option>').val('-1').text('New Row'));
+                }
+            });
+
+            // When add an unit, verify the conditions before adding it.
+            $('.bo-form-add-unit').submit(function(e) {
+                let i;
+                const row = parseInt($(this).find('[name=row]').val());
+                const unit = Unit.fromName($(this).find('[name=unit]').val());
+                const time = (parseInt($(this).find('[name=minuts]').val()) || 0) * 60 + 
+                                (parseInt($(this).find('[name=seconds]').val()) || 0);
+                let ok = false;
+
+                console.log(time);
+
+                // Don't submit form
+                e.preventDefault();
+
+                // Run simulation until desired time and check if it is possible                        
+                simulation.goToSecond(time);
+
+                if(simulation.hasEnoughResourcesForUnit(unit)) {
+                    // Enough resources but didn't checked if there is a building
+                    // Check now if there is an avalaible building
+                    // This now depend not on the simulation but on the build order
+                    // The difference is that the simulation is not exact, but the timing can be known exactly
+                    // And we can know if the queue is empty.
+
+                    /* row = -1 => unit is built in a new row */
+                    if(row === -1) {
+                        bo.addAction(new Build(time, unit.getName()));
+                        ok = true;
+                    } else {
+
+                        // Check there is space to produce the unit at this point of time
+                        if(bo.getActions()[row].isEmptySpaceBetween(time, time + unit.getCost().time)) {
+                            bo.getActions()[row].addAction(new Build(time, unit.getName()));
+                            ok = true;
+                        }
+                    }                            
+                }
+                
+                if(ok) {
+                    bo.notify('change');
+                }
+                else {
+                    alert("can't");
+                }
+            })
+
+            // Initialise with fake change to populate the row with the first unit selected
+            .find('[name=unit]').change();
+
+
+
+            /* Slider for the timing of the simulation */
+            $('#timeline input[type=range]').change(function(e) {
+                const time = parseInt($(this).val());
+
+                simulation.goToSecond(time);
+
+                ['time', 'mineral', 'gas', 'supply', 'max_supply', 'mineral_harvesters', 'gas_harvesters'].forEach(function(attr) {
+                    let value = simulation[attr];
+
+                    if(typeof value === 'number') {
+                        value = Math.round(value);
+                    }
+                    if(attr === 'time') {
+                        value = formatTime(value);
+                    }
+
+                    $('#timeline').find('.' + attr).text(value);
+                });
+            });
+            
+            // Force initialise UI
+            bo.notify('change');
+        });
+    });
+});
